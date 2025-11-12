520457db7ab2fd38ed3c80c32887ba29b9f3bade
Ethan Patterson <pattee14@mcmaster.ca>
2025-11-11 19:54:00 -0500

    add grid and shapes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,7 +25,16 @@ export default defineConfig(
     },
     rules: {
       ...eslintPluginReactHooks.configs.recommended.rules,
-      ...eslintPluginReactRefresh.configs.vite.rules
+      ...eslintPluginReactRefresh.configs.vite.rules,
+      'no-unused-vars': 'off',
+      'no-console': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'react/no-unescaped-entities': 'off',
+
+      'react/display-name': 'off',
+      'react/jsx-curly-brace-presence': ['warn', { props: 'never', children: 'never' }],
+      'trailing-comma': 'on',
+      'semi': ['error', 'always']
     }
   },
   eslintConfigPrettier
