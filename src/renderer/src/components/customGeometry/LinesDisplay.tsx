912edd5088aad1bbe04f3c137295e612dfb5fc01
HussainM3 <hussainmohd2003@outlook.com>
2025-11-19 23:19:02 -0500

    updated line so its position is correct relative to the grid

--- src/renderer/src/components/customGeometry/LinesDisplay.tsx
+++ src/renderer/src/components/customGeometry/LinesDisplay.tsx
@@ -7,14 +7,15 @@ const LinesDisplay = () => {
     <>
       {lines.map((line, index) => {
         return (
-          <svg key={`line-${index}`} style={{ position: 'absolute', pointerEvents: 'none' }}>
+          <svg key={`line-${index}`} style={{ position: 'absolute', pointerEvents: 'none', width: "100%", height: "100%"}}>
             <line
+              key={index}
               x1={line.position1.x}
               y1={line.position1.y}
               x2={line.position2.x}
               y2={line.position2.y}
               stroke="rgba(0,0,255,0.7)" // Line color, change as needed
-              strokeWidth={line.size} // You can customize the line thickness
+              strokeWidth={line.size-13} // You can customize the line thickness
             />
           </svg>
         );
