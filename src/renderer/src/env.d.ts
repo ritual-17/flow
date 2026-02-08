15ba56dbca87fb4289c459f29220036518fed7fe
Oliverzzzzz <zhaoc59@mcmaster.ca>
2026-02-08 13:31:22 -0500

    311:Update src/renderer/src/env.d.ts for adding window API

--- src/renderer/src/env.d.ts
+++ src/renderer/src/env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="vite/client" />
+
+export {};
+
+type SaveArgs = { contents: string; filePath?: string | null };
+type SaveResult = { filePath: string | null };
+type OpenResult = { filePath: string; contents: string } | null;
+
+declare global {
+  interface Window {
+    api: {
+      flowFS: {
+        open: () => Promise<OpenResult>;
+        save: (args: SaveArgs) => Promise<SaveResult>;
+      };
+    };
+  }
+}
