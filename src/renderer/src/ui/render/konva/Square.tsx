1c1af5ad04a691f17a1325f4916a589732cbf028
doanj2 <jeffrey.a.doan@gmail.com>
2026-02-10 01:03:18 -0500

    Fixed prop-type warning

--- src/renderer/src/ui/render/konva/Square.tsx
+++ src/renderer/src/ui/render/konva/Square.tsx
@@ -1,3 +1,5 @@
+// disabled prop-type warning because typescript knows shape is a DomainSquare
+/* eslint-disable react/prop-types */
 import { Square as DomainSquare } from '@renderer/core/geometry/shapes/Square';
 import { ShapeComponent } from '@renderer/ui/render/konva/ShapeResolver';
 import { Rect as KonvaSquare } from 'react-konva';
