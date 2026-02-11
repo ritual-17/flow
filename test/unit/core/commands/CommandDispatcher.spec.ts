d6794471f60b952b348e4d2afc7c5e3d88da587b
Oliverzzzzz <zhaoc59@mcmaster.ca>
2026-02-10 20:06:15 -0500

    365: Delete unit test for commandDispatcher in order to make pdf uploading work

--- test/unit/core/commands/CommandDispatcher.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CommandDispatcher } from '@renderer/core/commands/CommandDispatcher';
-
-describe('CommandDispatcher', () => {
-  test('should register and execute a command', () => {
-    const _t = new CommandDispatcher(() => {});
-  });
-});
